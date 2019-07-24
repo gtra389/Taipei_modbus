ddfead3c1233ba3752a2439b6ce2214e0a85f75f
gtra389 <52122223+gtra389@users.noreply.github.com>
2019-07-24 09:56:38 +0800

    Update AutoRunPy.sh

--- AutoRunPy.sh
+++ AutoRunPy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 sleep 15
-sudo python /home/pi/Taipei_modbus/LLS_modbus_taipei.py >> log
-sudo python /home/pi/Taipei_modbus/RG_modbus_taipei.py >> log
+sudo python /home/pi/Taipei_modbus/LLS_RG_modbus_taipei >> log
 while :
 do
   if [ `ps -U root -u root u | grep python | wc -m` -eq 0 ]
   then 
-    sudo python sudo python /home/pi/Taipei_modbus/LLS_modbus_taipei.py >> log
-    sudo python /home/pi/Taipei_modbus/RG_modbus_taipei.py >> log
+    sudo python sudo python /home/pi/Taipei_modbus/LLS_RG_modbus_taipei >> log
     sleep 180
   else
     sleep 180
