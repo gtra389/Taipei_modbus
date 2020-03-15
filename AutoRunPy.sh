1f2bed9b05c863a7b57e84119c07d99889f37def
gtra389 <52122223+gtra389@users.noreply.github.com>
2020-03-15 22:52:33 +0800

    Update AutoRunPy.sh

--- AutoRunPy.sh
+++ AutoRunPy.sh
@@ -7,10 +7,9 @@
 # do
 if [ `ps -U root -u root u | grep python | wc -m` -eq 0 ]
   then 
-    sudo python /home/pi/Taipei_modbus/LLS_RG_modbus_taipei.py
-    sudo python /home/pi/Taipei_modbus/raingage_interrupt.py
-    sleep 180
+    sudo python /home/pi/Taipei_modbus/taipei_modbus_v2.py
+    sleep 1
   else
-    sleep 180
+    sleep 1
 fi
 # done
