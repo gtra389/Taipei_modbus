02b54324dc9255be8881ee544b6a2ef01a9f3748
gtra389 <52122223+gtra389@users.noreply.github.com>
2020-04-23 19:27:45 +0800

    Add files via upload

--- raingauge_read.py
+++ raingauge_read.py
@@ -10,22 +10,32 @@
 # Including
 import minimalmodbus
 import time
+import RPi.GPIO as GPIO
+
+# Setting RS485 CAN HAT
+EN_485 =  4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(EN_485,GPIO.OUT)
+GPIO.output(EN_485,GPIO.HIGH)
 
 # Definition of variable
 minimalmodbus.BAUDRATE = 19200
 rg_slaveAddr = 2
-devName = '/dev/ttyUSB0' # RPI    format
-comName = 'COM3'         # Window format
+# devName = '/dev/ttyUSB0' # RPI  format via USB
+devName = "/dev/serial0" # pi zero
+# devName = "/dev/ttyS0"   # using RPI via  RS485 CAN HAT # Raspberry Pi 3B+
+# comName = 'COM3'         # Window format
 
-# instrument_2 = minimalmodbus.Instrument(devName, rg_slaveAddr) 
-instrument_2 = minimalmodbus.Instrument(comName, rg_slaveAddr) 
-while True:
-    try:
+try:
+    while True:
+        instrument_2 = minimalmodbus.Instrument(devName, rg_slaveAddr)
+        #instrument_2 = minimalmodbus.Instrument(comName, rg_slaveAddr) 
         # Register number, number of decimals, function code
         rf_realVal = instrument_2.read_register(0, 0, 4)
         print("rf_readVal: {}".format(rf_realVal))
-        time.sleep(1) # Wait for 1 second
-    except KeyboardInterrupt:
-        print("Stop!")
         instrument_2.close_port_after_each_call = True
-        break
+        time.sleep(1) # Wait for 1 second
+except KeyboardInterrupt:
+    print("Stop!")
+    instrument_2.close_port_after_each_call = True
+    break
