d721f7fb860fff042a1d4fb17f1c3e1efba9229b
gtra389 <52122223+gtra389@users.noreply.github.com>
2020-04-23 19:15:03 +0800

    Add files via upload

---
+++ raingauge_read.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Available device: Tipping-bucket Rain gauge
+# Product number: SMR29-C
+# Serial communication protocol: RS485
+# Purpose:
+# Test the comunication between devices and loggers
+#
+
+# Including
+import minimalmodbus
+import time
+
+# Definition of variable
+minimalmodbus.BAUDRATE = 19200
+rg_slaveAddr = 2
+devName = '/dev/ttyUSB0' # RPI    format
+comName = 'COM3'         # Window format
+
+# instrument_2 = minimalmodbus.Instrument(devName, rg_slaveAddr) 
+instrument_2 = minimalmodbus.Instrument(comName, rg_slaveAddr) 
+while True:
+    try:
+        # Register number, number of decimals, function code
+        rf_realVal = instrument_2.read_register(0, 0, 4)
+        print("rf_readVal: {}".format(rf_realVal))
+        time.sleep(1) # Wait for 1 second
+    except KeyboardInterrupt:
+        print("Stop!")
+        instrument_2.close_port_after_each_call = True
+        break
